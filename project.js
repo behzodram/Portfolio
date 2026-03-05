9da85ca361a71d66460078f4673b5584cab59173
Behzod <behzodmx27@gmail.com>
2026-03-05 15:12:55 +0500

    v 1.0.1 => project.js added

---
+++ project.js
@@ -0,0 +1,74 @@
+
+// Projects Data
+const projects = [
+    {
+        title: "ColorDetector",
+        description: "Android Game application. 10 Levels. You can confuse as levels up. On each level you can see your score",
+        technologies: ["HTML", "CSS", "JavaScript", "Firebase"],
+        image: "assets/images/ColorDetector.png",
+        githubLink: "https://github.com/behzodram/ColorDetector",
+        demoLink: "https://behzodram.github.io/ColorDetector/",
+        downloadLink: "assets/downloads/ColorDetector.apk",
+        directDownload: true
+    },
+    {
+        title: "PortfolioOfNodirbek",
+        description: "This is my portfolio website. It includes Nodirbek's projects, skills, and contact information.",
+        technologies: ["HTML", "CSS", "JavaScript", "Firebase"],
+        image: "assets/images/PortfolioOfNodirbek.png",
+        githubLink: "https://github.com/behzodram/PortfolioOfNodirbek",
+        demoLink: "https://behzodram.github.io/PortfolioOfNodirbek/",
+        downloadLink: "assets/downloads/PortfolioOfNodirbek.apk",
+        directDownload: true
+    },
+    {
+        title: "BallCatcher",
+        description: "Android Game application. Just catch the balls and see your analitics with Leaderboard.",
+        technologies: ["HTML", "CSS", "JavaScript", "Firebase"],
+        image: "assets/images/BallCatcher.png",
+        githubLink: "https://github.com/behzodram/BallCatcher",
+        demoLink: "https://behzodram.github.io/BallCatcher/",
+        downloadLink: "assets/downloads/BallCatcher.apk",
+        directDownload: true
+    },
+    {
+        title: "PuncturedBall",
+        description: "Android Game application. Just touch the ball on screen.",
+        technologies: ["HTML", "CSS", "JavaScript", "Firebase"],
+        image: "assets/images/PuncturedBall.png",
+        githubLink: "https://github.com/behzodram/PuncturedBall",
+        demoLink: "https://behzodram.github.io/PuncturedBall/",
+        downloadLink: "assets/downloads/PuncturedBall.apk",
+        directDownload: true
+    },
+    {
+        title: "TruckDispatch",
+        description: "Android application that is useful for dispatchers.",
+        technologies: ["HTML", "CSS", "JavaScript", "Firebase"],
+        image: "assets/images/TruckDispatch.png",
+        githubLink: "https://github.com/behzodram/TruckDispatch/",
+        demoLink: "https://behzodram.github.io/TruckDispatch/",
+        downloadLink: "assets/downloads/TruckDispatch.apk",
+        directDownload: true
+    },
+    {
+        title: "Control",
+        description: "Control homework application with firebase database integration.",
+        technologies: ["HTML", "Firebase", "JavaScript"],
+        image: "assets/images/Control.png",
+        githubLink: "https://github.com/behzodram/Control/",
+        demoLink: "https://behzodram.github.io/Control/",
+        downloadLink: "assets/downloads/Control.apk",
+        directDownload: true
+    },
+    {
+        title: "Time Picker",
+        description: "Android application that displays any time to pick the screen.",
+        technologies: ["HTML", "CSS", "JavaScript"],
+        image: "assets/images/PickTime.png",
+        githubLink: "https://github.com/behzodram/PickTime",
+        demoLink: "https://behzodram.github.io/PickTime/",
+        downloadLink: "assets/downloads/PickTime.apk",
+        directDownload: true
+    },
+];
